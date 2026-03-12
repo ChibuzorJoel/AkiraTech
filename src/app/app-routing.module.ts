a1be2e0d742598e9cc545b2ed37d55fb8ac3ecad
ChibuzorJoel <chibuzoronyedikachi72@gmail.com>
2026-03-12 21:53:44 +0100

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { HomeComponent }      from './pages/home/home.component';
 import { AboutComponent }     from './pages/about/about.component';
 import { ServicesComponent }  from './pages/services/services.component';
 import { ContactComponent }   from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '',           component: HomeComponent },
   { path: 'about',      component: AboutComponent },
   { path: 'services',   component: ServicesComponent },
-  
+  { path: 'projects',   component: PortfolioComponent },
   { path: 'contact',    component: ContactComponent },
   { path: '**',         redirectTo: '' }                  
 ];
